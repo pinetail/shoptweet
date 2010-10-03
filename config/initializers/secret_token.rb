99004f94312d42c2eaf94b5eda3aab790da61c6d
Shinichi Matsuo <civic.typexs@gmail.com>
2010-10-03 15:19:59 +0900

    add rails source

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittenpo::Application.config.secret_token = 'bd652958416bf91362cb3f231052e62e23ff265bba84edccbdb6ee0361491bbf4bad24b13785f8bda2e0366d51ab34b652470e5d646d6315493c7ccf010e0e30'
